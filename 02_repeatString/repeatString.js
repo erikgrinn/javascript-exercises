abf72c09d01823b93b02cc63e3edb8d3690d6221
Erik Grinn <grinn.erik@gmail.com>
2024-08-15 15:22:29 -0400

    all passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let myString = String(string)
+    let myNum = Number(num)
 
+    if (myNum === 0) {
+        return ''
+    } else if (myNum <0) {
+        return 'ERROR'
+    }
+
+    let cumulativeString = '';
+    for (i=0; i<myNum; i++) {
+        cumulativeString = cumulativeString + myString
+    }
+    return cumulativeString
 };
 
 // Do not edit below this line
