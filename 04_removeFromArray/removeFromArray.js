2881c05060863162cdf323b45661952f4f7001cd
Erik Grinn <grinn.erik@gmail.com>
2024-08-15 16:10:37 -0400

    append empty array with values not in remove args

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputArray, ...inputArgs) {
+    let filteredArray = [];
+
+    for (let i = 0; i < inputArray.length; i++) {
+        let element = inputArray[i];
+        if (inputArgs.indexOf(element) === -1) {
+            filteredArray.push(element);
+        }
+    }
+
+    return filteredArray;
 };
 
 // Do not edit below this line
