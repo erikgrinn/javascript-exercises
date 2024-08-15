4fe2f42688986d53f3cd023ed7a3dc85571efa83
Erik Grinn <grinn.erik@gmail.com>
2024-08-15 17:10:33 -0400

    all passed

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,17 @@
-const leapYears = function() {
+const leapYears = function(inputYear) {
+    let myYear = inputYear
 
+    if (myYear%4 == 0) {
+        if ((myYear%100 != 0) || myYear%400 == 0) {
+           return true 
+        } else {
+            return false
+        }
+    } else {
+        return false
+    }
 };
+
 
 // Do not edit below this line
 module.exports = leapYears;
