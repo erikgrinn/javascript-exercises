f1aa5606a5954747b24a1c1781cc4b470fe3f51d
Erik Grinn <grinn.erik@gmail.com>
2024-08-15 17:17:46 -0400

    added shortened version (commented)

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -12,6 +12,9 @@ const leapYears = function(inputYear) {
     }
 };
 
+// or...
+// return (inputYear % 4 === 0 && (inputYear % 100 !== 0 || inputYear % 400 === 0));
+// }
 
 // Do not edit below this line
 module.exports = leapYears;
