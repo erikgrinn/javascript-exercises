0db059251ecc04d68e54e258954d121974ff76a1
Erik Grinn <grinn.erik@gmail.com>
2024-08-15 15:29:45 -0400

    reverse string two methods

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,19 @@
-const reverseString = function() {
-
+const reverseString = function(inputString) {
+    let myString = String(inputString)
+    let reversedString = '';
+    for (let i = myString.length - 1; i >= 0; i--) {
+        reversedString += myString[i];
+    }
+    return reversedString;
 };
+
+// or...
+
+// function reverseString(inputString) {
+//     return inputString.split('').reverse().join('');
+// };
+
+
 
 // Do not edit below this line
 module.exports = reverseString;
