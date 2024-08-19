e863b3273d9c00a36d3023e903d5f29be2001fd0
Erik Grinn <grinn.erik@gmail.com>
2024-08-19 14:24:10 -0400

    all passed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    // Convert the string to lowercase and remove non-alphanumeric characters
+    const cleanStr = str
+        .toLowerCase()
+        .split('')
+        .filter(char => /[a-z0-9]/.test(char));
 
+    // Use reduce to check if the string is the same forwards and backwards
+    const isPalin = cleanStr.reduce((acc, char, index) => {
+        return acc && (char === cleanStr[cleanStr.length - 1 - index]);
+    }, true);
+
+    return isPalin;
 };
 
 // Do not edit below this line
