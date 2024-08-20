de793363f531a45a58a066d03dbab576169497b8
Erik Grinn <grinn.erik@gmail.com>
2024-08-19 21:30:28 -0400

    all passed

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    titleArr = []
+    for (let i=0; i<books.length; i++) {
+        titleArr.push(books[i].title)
+    }
+    return titleArr
 };
 
 // Do not edit below this line
