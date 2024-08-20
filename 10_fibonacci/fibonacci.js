c29500db940d28d97a860a45633d453afaf66c1f
Erik Grinn <grinn.erik@gmail.com>
2024-08-19 20:57:18 -0400

    all passed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(nthNumber) {
 
+    if (nthNumber < 0) return 'OOPS'
+
+    let fibArr = [0, 1];
+    for (let i = 1; i < nthNumber; i++) {
+        let nextNum = fibArr[i] + fibArr[i - 1];
+        fibArr.push(nextNum);
+    }
+    return fibArr[nthNumber];
 };
 
 // Do not edit below this line
